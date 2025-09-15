d8ba05b57f7e844c89ca0038182ff2611d481333
Will Gibson <william.m.gibson234@gmail.com>
2025-09-15 13:40:04 -0400

    server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const authRoutes = require('./routes/auth.js');
+const topicRoutes = require('./routes/topics')
 
 const app = express();
 const PORT = 3000;
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/api/auth',authRoutes);
+app.use('/api/topics',topicRoutes);
 
 app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname,'public','yourtopfives.html'));
